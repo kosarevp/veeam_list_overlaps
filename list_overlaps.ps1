2713b325df917781517c33cf517c7f72e4ce4dae
kosarevp <34346736+kosarevp@users.noreply.github.com>
2021-09-29 16:52:31 +0300

    Create list_overlaps.ps1

---
+++ list_overlaps.ps1
@@ -0,0 +1,22 @@
+# List overlapping backup sessions 
+
+Import-Module -Name 'Veeam.Backup.PowerShell' -DisableNameChecking
+$BSessions = Get-VBRBackupSession
+
+while ($BSessions) {
+    $CurSession, $BSessions = $BSessions
+
+    $Overlaps = @()
+    foreach ($Session in $BSessions) {
+        if ($CurSession.CreationTime -ge $Session.CreationTime -and $CurSession.CreationTime -le $Session.EndTime -or $CurSession.EndTime -ge $Session.CreationTime -and $CurSession.EndTime -le $Session.EndTime)
+        {
+            $Overlaps += $Session
+        }
+    }
+    if ($Overlaps) {
+        Write-Host ($CurSession.Name + ' (creation: ' + $CurSession.CreationTime + ', end: ' + $CurSession.EndTime + ') overlaps with following backup sessions:')
+        foreach ($item in $Overlaps) {
+            Write-Host '-->', $item.Name, 'creation:', $item.CreationTime, 'end:', $item.EndTime    
+        }
+    }
+}
